f1e8cc0b0bba3afb1e15c294a733d79e13060e7b
AchilihuDaniel07 <achilihudaniel53@gmail.com>
2022-08-17 12:07:28 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import MyComponent from './MyComponent';
 import MyClassComponent from './MyClassComponent';
-import MyCounter from './MyCounter';
 import MyTodolist from './MyTodolist';
-// import logo from './logo.svg';
 import './App.css';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 
 function App() {
   const info ="Information about Developer"
   return (
     <div className="App">
       <header className="App-header">
-            {/* <MyComponent info={info} 
-                name="Daniel" 
-                age="19" 
-                career="Web developer" 
-                tribe="Igbo" 
-                religion="Christain" 
-            /> */}
+           
 
             {/* <MyClassComponent 
                 name ="Kesh"
